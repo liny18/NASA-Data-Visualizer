2f0d0357d899803f68a9c761bd6f448e1cf2642b
Samoo <Samoooomei@gmail.com>
2023-04-21 16:48:50 -0400

    edited API to send Wind Speed 50M

--- Project/server/server.js
+++ Project/server/server.js
@@ -19,14 +19,14 @@ app.get('/api/loc/:lat/:long', async (req, res) => {
         .then((final) => res.status(200).send(final));
 });
 
-app.get('/api/nasa/:lat/:long/:start/:end', async (req, res) => {
+app.get('/api/nasa/:start/:end/:lat/:long', async (req, res) => {
     const lat = req.params.lat;
     const long = req.params.long;
     const start = req.params.start;
     const end = req.params.end;
-    const url = `https://power.larc.nasa.gov/api/application/windrose/point?start=${start}&end=${end}&latitude=${lat}&longitude=${long}`;
+    const url = `https://power.larc.nasa.gov/api/temporal/climatology/point?start=${start}&end=${end}&latitude=${lat}&longitude=${long}&community=re&parameters=WS50M&format=json&header=true`;
 
     fetch(url)
         .then((result) => result.json())
-        .then((final) => res.status(200).send(final));
+        .then((final) => res.status(200).send(final.properties.parameter.WS50M));
 });
