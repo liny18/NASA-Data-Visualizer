6c25217be5d3ade408f8cc8bf0f3673c73d637b6
liny18 <liny18@rpi.edu>
2023-04-24 12:28:48 -0400

    fix cannot get error on page refresh

--- Project/client/src/Components/Header.tsx
+++ Project/client/src/Components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <div className="header-container fixed z-50 flex w-full flex-col items-center justify-between border-[2px] border-gray-300 bg-gray-50 shadow-[5px_5px_2px_0px_#d1d5db] md:flex-row">
       <div className="nav-container flex w-full items-center justify-between border-r-[2px] border-gray-300 py-5 px-5 md:w-5/6 md:px-10">
-        <Link to="/">
+        <Link to="/node/">
           <div className="logo-container flex items-center rounded-3xl border-2 border-amber-200 bg-amber-100 p-1 shadow-[5px_5px_2px_0px_#fcd34d] hover:bg-amber-200">
             <img
               src={logo}
